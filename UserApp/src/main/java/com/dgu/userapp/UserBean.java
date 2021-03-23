0e173dc8adc5637edda67cf0c1177ff5abad6f98
ysh9693 <36917355+ysh9693@users.noreply.github.com>
2021-03-24 01:29:56 +0900

    Add files via upload

---
+++ UserApp/src/main/java/com/dgu/userapp/UserBean.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dgu.userapp;
+
+public class UserBean {
+    String userID;
+    String userPassword;
+    String userName;
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    
+}
---
+++ UserApp/src/main/java/com/dgu/userapp/UserDao.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dgu.userapp;
+
+import java.sql.*;
+
+public class UserDao {
+    private Connection conn; //자바와 데이터베이스를 연결
+    private PreparedStatement pstmt; //쿼리문 대기 및 설정
+    private ResultSet rs; //결과값 받아오기
+    
+    //기본 생성자
+    //UserDao가 실행되면 자동으로 생성되는 부분
+    //메소드마다 반복되는 코드를 이곳에 넣으면 코드가 간소화된다
+    public UserDao(){
+        try{
+            String url="jdbc:oracle:thin:@dalma.dongguk.ac.kr:1521:stud1";
+            String user ="ysh9693";
+            String password = "1234";
+            String sql2 = "select * from users where id=?";
+            Class.forName("oracle.jdbc.Driver");
+            conn = DriverManager.getConnection(url,user,password);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+	public int login(String userID, String userPassword) {
+		String sql = "select userPassword from user where userID = ?";
+		try {
+			pstmt = conn.prepareStatement(sql); //sql쿼리문을 대기 시킨다
+			pstmt.setString(1, userID); //첫번째 '?'에 매개변수로 받아온 'userID'를 대입
+			rs = pstmt.executeQuery(); //쿼리를 실행한 결과를 rs에 저장
+			if(rs.next()) {
+				if(rs.getString(1).equals(userPassword)) {
+					return 1; //로그인 성공
+				}else
+					return 0; //비밀번호 틀림
+			}
+			return -1; //아이디 없음
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -2; //오류
+	}
+         
+}
